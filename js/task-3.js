03c9e4b226f760ed442c9609813dccecf0d246bb
venuscorpua <venuscorpua@gmail.com>
2024-05-29 16:21:15 +0300

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,15 @@
-function checkForSpam(message){
-    message = message.toLowerCase();
-    return message.includes("spam") || message.includes("sale")
+function filterArray(numbers, value){
+    arrayNew = [];
+   for (const el of numbers){
+        if(el > value){
+            arrayNew.push(el)
+        }
+   }
+return    arrayNew
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
